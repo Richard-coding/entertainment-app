b35d08faa19731d960bcc7936ef7e02a7ff92953
Richard-coding <richardcoding@outlook.com>
2026-08-18 14:46:55 -0300

    feat: implement responsive trending and regular collections

--- src/components/EntertainmentCard.tsx
+++ src/components/EntertainmentCard.tsx
@@ -10,58 +10,100 @@ const EntertainmentCard = ({
   onChangeBookmark,
   variant,
 }: EntertainmentCardProps) => {
-  const isTrendingVariant = variant === "trending";
+  const isTrending = variant === "trending";
+
   const cardStyle: EntertainmentCardStyle = {
     "--bg-small": `url(${
-      isTrendingVariant ? movie.thumbnail.trending?.small : movie.thumbnail.regular.small
+      isTrending
+        ? movie.thumbnail.trending?.small
+        : movie.thumbnail.regular.small
     })`,
     "--bg-large": `url(${
-      isTrendingVariant ?movie.thumbnail.trending?.large : movie.thumbnail.regular.large
+      isTrending
+        ? movie.thumbnail.trending?.large
+        : movie.thumbnail.regular.large
     })`,
   };
 
-  const onToggleBookmark = (id: number) => {
-    onChangeBookmark(id);
-  };
+  if (isTrending) {
+    return (
+      <article
+        style={cardStyle}
+        className=" relative flex h-[140px] w-[240px] shrink-0 flex-col justify-between overflow-hidden rounded-lg bg-[image:var(--bg-small)] bg-cover bg-center p-2
+ md:h-[230px] md:w-[470px] md:bg-[image:var(--bg-large)] md:p-4
+        "
+      >
+        <div className="ml-auto">
+          <BookmarkedButton
+            onToggleBookmark={() => onChangeBookmark(movie.id)}
+            isBookmarked={movie.isBookmarked}
+          />
+        </div>
+
+        <div className="text-white">
+          <div
+            className="
+              mb-1 flex items-center gap-2
+              text-preset-5-mobile opacity-75
+              md:text-preset-4
+            "
+          >
+            <span>{movie.year}</span>
+            <span>•</span>
+
+            <span className="flex items-center gap-1.5">
+              <Icons category={movie.category} />
+              {movie.category}
+            </span>
+
+            <span>•</span>
+            <span>{movie.rating}</span>
+          </div>
+
+          <h2 className="text-preset-3-mobile md:text-preset-2-medium">
+            {movie.title}
+          </h2>
+        </div>
+      </article>
+    );
+  }
 
   return (
-    <>
-      {variant === "trending" ? (
-        <div
-          style={cardStyle}
-          className="bg-[image:var(--bg-small)] md:bg-[image:var(--bg-large)] lg:bg-[image:var(--bg-large)] bg-cover rounded-lg"
-        >
-          <div className="p-2 min-w-60 min-h-35 flex flex-col justify-between ">
-            <div className="ml-auto">
-              <BookmarkedButton
-                onToggleBookmark={() => onToggleBookmark(movie.id)}
-                isBookmarked={movie.isBookmarked}
-              />
-            </div>
+    <article className="w-full min-w-0">
+      <div
+        style={cardStyle}
+        className="mb-2 flex aspect-[164/110] w-full min-w-0 rounded-lg bg-[image:var(--bg-small)] bg-cover bg-center p-2 md:aspect-[280/174] md:bg-[image:var(--bg-large)]
+        "
+      >
+        <div className="ml-auto">
+          <BookmarkedButton
+            onToggleBookmark={() => onChangeBookmark(movie.id)}
+            isBookmarked={movie.isBookmarked}
+          />
+        </div>
+      </div>
 
-            <div className="pb-1.5 pl-2 text-white">
-              <div className="flex gap-1.5 items-center text-preset-5-mobile">
-                {movie.year} • <Icons category={movie.category} /> {movie.category} • {movie.rating}
-              </div>
-              <h2 className="text-preset-3-mobile">{movie.title}</h2>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div
-            style={cardStyle}
-            className="w-[164px] min-h-[110px] bg-[image:var(--bg-small)] md:bg-[image:var(--bg-large)] lg:bg-[image:var(--bg-large)] bg-cover rounded-lg"
-          ></div>
-          <div className="pb-1.5 pl-2">
-            <div className="flex gap-1.5 items-center">
-              {movie.year} • <Icons category={movie.category} /> {movie.category} • {movie.rating}
-            </div>
-            <h2>{movie.title}</h2>
-          </div>
-        </div>
-      )}
-    </>
+      <div
+        className="
+          mb-1 flex items-center gap-1.5
+          text-preset-6-mobile opacity-75
+          md:text-preset-5
+        "
+      >
+        <span>{movie.year}</span>
+        <span>•</span>
+
+        <span className="flex items-center gap-1.5">
+          <Icons category={movie.category} />
+          {movie.category}
+        </span>
+
+        <span>•</span>
+        <span>{movie.rating}</span>
+      </div>
+
+      <h2 className="text-preset-4-mobile md:text-preset-3">{movie.title}</h2>
+    </article>
   );
 };
 
--- src/components/EntertainmentCards.tsx
+++ src/components/EntertainmentCards.tsx
@@ -1,12 +1,48 @@
 import type { EntertainmentCardsProps } from "../types/entertainment";
 import EntertainmentCard from "./EntertainmentCard";
 
-const EntertainmentCards = ({ movies, onChangeBookmark, variant }: EntertainmentCardsProps) => {
+const EntertainmentCards = ({
+  movies,
+  onChangeBookmark,
+  variant,
+}: EntertainmentCardsProps) => {
+  if (variant === "trending") {
+    return (
+      <div
+        className="
+          flex w-full min-w-0 flex-nowrap gap-4 overflow-x-auto pb-1
+          md:gap-8
+          lg:gap-10
+          [&::-webkit-scrollbar]:hidden
+        "
+      >
+        {movies.map((movie) => (
+          <EntertainmentCard
+            key={movie.id}
+            movie={movie}
+            onChangeBookmark={onChangeBookmark}
+            variant="trending"
+          />
+        ))}
+      </div>
+    );
+  }
 
   return (
-    <div className="flex gap-4">
+    <div
+      className="
+        grid w-full min-w-0 grid-cols-2 gap-4
+        md:grid-cols-3 md:gap-x-7 md:gap-y-6
+        lg:grid-cols-4 lg:gap-x-10 lg:gap-y-8
+      "
+    >
       {movies.map((movie) => (
-        <EntertainmentCard key={movie.id} movie={movie} onChangeBookmark={onChangeBookmark} variant={variant}/>
+        <EntertainmentCard
+          key={movie.id}
+          movie={movie}
+          onChangeBookmark={onChangeBookmark}
+          variant="regular"
+        />
       ))}
     </div>
   );
