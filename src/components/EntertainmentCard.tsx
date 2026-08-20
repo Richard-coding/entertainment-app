f4b212c5703a03ecab043618421d717f56bd5c5b
Richard-coding <richardcoding@outlook.com>
2026-08-20 11:36:13 -0300

    feat: added hover img for movie start

--- src/components/EntertainmentCard.tsx
+++ src/components/EntertainmentCard.tsx
@@ -4,7 +4,7 @@ import type {
 } from "../types/entertainment";
 import BookmarkedButton from "./BookmarkedButton";
 import Icons from "./Icons";
-
+import PlayIcon from "../assets/icons/icon-play.svg";
 const EntertainmentCard = ({
   movie,
   onChangeBookmark,
@@ -29,10 +29,23 @@ const EntertainmentCard = ({
     return (
       <article
         style={cardStyle}
-        className=" relative flex h-[140px] w-[240px] shrink-0 flex-col justify-between overflow-hidden rounded-lg bg-[image:var(--bg-small)] bg-cover bg-center p-2
+        className="relative flex h-[140px] w-[240px] shrink-0 flex-col justify-between overflow-hidden rounded-lg bg-[image:var(--bg-small)] bg-cover bg-center p-2
  md:h-[230px] md:w-[470px] md:bg-[image:var(--bg-large)] md:p-4
-        "
+group"
       >
+        <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-10 cursor-pointer">
+          <div className="flex items-center text-white gap-4 border border-none rounded-[28.5px] p-[9px_27px_9px_9px]  bg-white/25">
+            <img
+              src={PlayIcon}
+              alt="Play"
+              className="w-28 h-12 md:w-auto md:h-auto"
+            />
+            <p className="text-preset-3-mobile md:text-preset-3 lg:text-preset-3">
+              Play
+            </p>
+          </div>
+        </div>
+
         <div className="ml-auto">
           <BookmarkedButton
             onToggleBookmark={() => onChangeBookmark(movie.id)}
@@ -72,9 +85,22 @@ const EntertainmentCard = ({
     <article className="w-full min-w-0">
       <div
         style={cardStyle}
-        className="mb-2 flex aspect-[164/110] w-full min-w-0 rounded-lg bg-[image:var(--bg-small)] bg-cover bg-center p-2 md:aspect-[280/174] md:bg-[image:var(--bg-large)]
+        className="relative mb-2 flex aspect-[164/110] w-full min-w-0 rounded-lg bg-[image:var(--bg-small)] bg-cover bg-center p-2 md:aspect-[280/174] md:bg-[image:var(--bg-large)] group
         "
       >
+        <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-10 cursor-pointer">
+          <div className="flex items-center text-white gap-4 border border-none rounded-[28.5px] p-[9px_27px_9px_9px]  bg-white/25">
+            <img
+              src={PlayIcon}
+              alt="Play"
+              className="w-28 h-12 md:w-auto md:h-auto"
+            />
+            <p className="text-preset-3-mobile md:text-preset-3 lg:text-preset-3">
+              Play
+            </p>
+          </div>
+        </div>
+
         <div className="ml-auto">
           <BookmarkedButton
             onToggleBookmark={() => onChangeBookmark(movie.id)}
